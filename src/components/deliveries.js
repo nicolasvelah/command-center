c7143d5ed52df0adb75a711cb1415a6e3b08725c
Nicolas Vela <nicolasvelah@gmail.com>
2019-02-28 15:38:29 -0500

    filtros entregas V1

--- src/components/deliveries.js
+++ src/components/deliveries.js
@@ -3,7 +3,8 @@ import moment from 'moment'
 import { DatePickerInput } from 'rc-datepicker'
 import InfiniteScroll from 'react-infinite-scroller'
 import axios from 'axios'
-import { getUser } from '../services/auth'
+import Select from 'react-select'
+import { getUser, logoutLocal } from '../services/auth'
 import Modal from './modal'
 import Task from './Task'
 //import ExcelDownload from './ExcelDownload'
@@ -12,6 +13,18 @@ import 'moment/locale/es.js'
 import 'rc-datepicker/lib/style.css'
 import '../assets/css/deliveries.css'
 
+const columnsAll = [
+  { label: 'Id', value: 0, id: 0 },
+  { label: 'Fecha', value: 1, id: 1 },
+  { label: 'Categoria', value: 2, id: 2 },
+  { label: 'Servicio', value: 3, id: 3 },
+  { label: 'Cliente', value: 4, id: 4 },
+  { label: 'Proveedor', value: 5, id: 5 },
+  { label: 'Operador', value: 6, id: 6 },
+  { label: 'Pais', value: 7, id: 7 },
+  { label: 'Ciudad', value: 8, id: 8 },
+  { label: 'Direccion', value: 9, id: 9 },
+]
 export default class Deliveries extends Component {
   constructor(props) {
     super(props)
@@ -25,15 +38,32 @@ export default class Deliveries extends Component {
         .startOf('month')
         .format('YYYY-MM-DDT00:00:20.719Z'),
       hasMoreItems: true,
+      showModal: false,
+      curTask: [],
+      messagesTask: [],
+      notesTask: [],
+      filterColumn: '',
+      filterKeyword: '',
+      filterColection: [],
     }
   }
 
   async componentDidMount() {
+    let filterColection = []
+    if (
+      typeof window !== 'undefined' &&
+      window.localStorage.getItem('filtercolection')
+    ) {
+      filterColection = JSON.parse(
+        window.localStorage.getItem('filtercolection')
+      )
+    }
+    if (filterColection.length > 0) {
+      this.setState({ filterColection: filterColection })
+    }
     await this.nextPage(false)
   }
   onChange = async (jsDate, type) => {
-    console.log(type)
-    console.log(moment(jsDate).format('YYYY-MM-DDT00:00:20.719Z'))
     if (type === 'from') {
       await this.setState({
         firstDate: moment(jsDate).format('YYYY-MM-DDT00:00:20.719Z'),
@@ -58,12 +88,9 @@ export default class Deliveries extends Component {
       } else {
         page = 1
       }
-      const url = `${
-        process.env.API_URL
-      }/orders/getOrdersDeliveries/${page}/limit/20`
-      console.log(url)
+
       const data = await axios.post(
-        url,
+        `${process.env.API_URL}/orders/getOrdersDeliveries/${page}/limit/20`,
         {
           from: this.state.firstDate,
           to: this.state.date,
@@ -95,17 +122,16 @@ export default class Deliveries extends Component {
         pages: data.data.pages,
         count: data.data.count,
         hasMoreItems: hasMoreItems,
-        showModal: false,
-        curTask: [],
-        messagesTask: [],
-        notesTask: [],
       })
 
       console.log('this.state.items', this.state.items)
 
       return data
     } catch (err) {
-      console.log(err.message)
+      if (err.response.status === 403) {
+        console.log('logour:', err.response.status)
+        logoutLocal()
+      }
       return []
     }
   }
@@ -209,6 +235,60 @@ export default class Deliveries extends Component {
     })
   }
 
+  addFilter = () => {
+    console.log('addFilter')
+    console.log(this.state.filterColumn)
+    console.log(this.state.filterKeyword)
+
+    let { filterColection } = this.state
+    filterColection.push({
+      column: this.state.filterColumn,
+      keyword: this.state.filterKeyword,
+    })
+    console.log('filterColection', filterColection)
+    window.localStorage.setItem(
+      'filtercolection',
+      JSON.stringify(filterColection)
+    )
+    this.setState({
+      filterColection,
+    })
+  }
+  setFilter = (option, id) => {
+    console.log('setFilter ID', id)
+    console.log('setFilter', option)
+
+    if (id === 'filterColumn') {
+      let { filterColumn } = this.state
+      filterColumn = option.label
+      this.setState({
+        filterColumn,
+      })
+    } else {
+      let { filterKeyword } = this.state
+      filterKeyword = option
+      this.setState({
+        filterKeyword,
+      })
+    }
+    console.log('filterColumn', this.state.filterColumn)
+    console.log('filterKeyword', this.state.filterKeyword)
+  }
+  deleteFilter = index => {
+    let { filterColection } = this.state
+    filterColection.map(item => {
+      if (index > -1) {
+        filterColection.splice(index, 1)
+      }
+      return item
+    })
+    window.localStorage.setItem(
+      'filtercolection',
+      JSON.stringify(filterColection)
+    )
+    this.setState({ filterColection })
+  }
+
   render() {
     const loader = (
       <div key="loader" className="loader">
@@ -221,6 +301,62 @@ export default class Deliveries extends Component {
       this.state.items
         .sort((a, b) => {
           return b.id - a.id
+        })
+        .filter(item => {
+          const { filterColection } = this.state
+          let response = true
+          let responseArray = []
+          if (filterColection.length > 0) {
+            response = false
+            filterColection.map(filter => {
+              if (filter.column === 'Id') {
+                if (Number(item.id) === Number(filter.keyword)) {
+                  responseArray.push(true)
+                }
+              } else if (filter.column === 'Categoria') {
+                if (item.service.categories.name === filter.keyword) {
+                  responseArray.push(true)
+                }
+              } else if (filter.column === 'Servicio') {
+                if (item.service.name === filter.keyword) {
+                  responseArray.push(true)
+                }
+              } else if (filter.column === 'Cliente') {
+                if (
+                  item.client.name + ' ' + item.client.lastName ===
+                  filter.keyword
+                ) {
+                  responseArray.push(true)
+                }
+              } else if (filter.column === 'Proveedor') {
+                if (item.provider.busnessName === filter.keyword) {
+                  responseArray.push(true)
+                }
+              } else if (filter.column === 'Operador') {
+                if (
+                  item.operator.name + ' ' + item.operator.lastName ===
+                  filter.keyword
+                ) {
+                  responseArray.push(true)
+                }
+              }
+              console.log('responseArray: ', responseArray)
+              if (responseArray.length === filterColection.length) {
+                responseArray.map(resp => {
+                  if (resp) {
+                    response = true
+                  }
+                  return resp
+                })
+              } else {
+                response = false
+              }
+
+              return filter
+            })
+          }
+
+          return response
         })
         .map((item, i) => {
           items.push(
@@ -316,12 +452,52 @@ export default class Deliveries extends Component {
             />
           </div>
           <div className="inputGroupContainer">
-            <div>Columna</div>
-            <div>Keyword</div>
-            <button className="btm addFilter">Añadir Filtro</button>
+            <div>
+              <Select
+                className="input"
+                classNamePrefix="column"
+                placeholder="Columna"
+                isClearable={true}
+                isSearchable={true}
+                name="column"
+                onChange={option => this.setFilter(option, 'filterColumn')}
+                options={columnsAll}
+              />
+            </div>
+            <div>
+              <input
+                defaultValue={''}
+                placeholder="Ingresa una palabra clave"
+                className="input keywordFilter"
+                onChange={e => this.setFilter(e.target.value, 'filterKeyword')}
+              />
+            </div>
+            <button className="btm addFilter" onClick={this.addFilter}>
+              Añadir Filtro
+            </button>
           </div>
           <div />
         </div>
+        {this.state.filterColection.length > 0 ? (
+          <div className="filterTrack">
+            <h4>Filtros activos</h4>
+            <div className="activesFilters">
+              {this.state.filterColection.map((item, i) => (
+                <div key={i} className="filterItem">
+                  <b>Columna:</b> {item.column} <b>/ Keyword:</b> {item.keyword}{' '}
+                  <span
+                    className="deleteFilter"
+                    onClick={e => this.deleteFilter(i)}
+                  >
+                    X
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
         <div>
           {this.state.items !== null ? (
             <InfiniteScroll
@@ -333,16 +509,9 @@ export default class Deliveries extends Component {
               <table>
                 <tbody>
                   <tr>
-                    <th>Id</th>
-                    <th>Fecha</th>
-                    <th>Categoria</th>
-                    <th>Servicio</th>
-                    <th>Cliente</th>
-                    <th>Proveedor</th>
-                    <th>Operador</th>
-                    <th>Pais</th>
-                    <th>Ciudad</th>
-                    <th>Direccion</th>
+                    {columnsAll.map(item => (
+                      <th key={item.id}>{item.label}</th>
+                    ))}
                     <th>Aciones</th>
                   </tr>
                   {items}
